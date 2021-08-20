f6c34e866f8a8a26a8960e572c11f8da593103d4
2ndas <sandro.shanshiashvili.1@btu.edu.ge>
2021-08-20 11:58:12 +0400

    Initial Commit

---
+++ taxSplitter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  taxSplitter
+//
+//  Created by sandro shanshiashvili on 20.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
